8cd5737e4f6010632559f475d8b9809c00ae1aef
Oscar Fritzsche <oscar.fritzsche@dart-racing.de>
2025-01-29 00:32:53 +0100

    done: 24-35

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -9,12 +9,12 @@ const std = @import("std");
 
 const NumError = error{IllegalNumber};
 
-pub fn main() void {
+pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
-    const my_num: u32 = getNumber();
+    const my_num: NumError!u32 = try getNumber();
 
-    try stdout.print("my_num={}\n", .{my_num});
+    try stdout.print("my_num={!}\n", .{my_num});
 }
 
 // This function is obviously weird and non-functional. But you will not be changing it for this quiz.
